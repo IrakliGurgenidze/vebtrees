b7d9b3f2f745577f5526adc34d83d03f836d4125
IrakliGurgenidze <46796069+IrakliGurgenidze@users.noreply.github.com>
2025-02-05 17:18:07 -0500

    Update test.cc

--- test.cc
+++ test.cc
@@ -1,5 +1,10 @@
-// Author: Prashant Pandey <prashant.pandey@utah.edu>
-// For use in CS6968 & CS5968
+/*
+ * Irakli Gurgenidze
+ * CS 7280
+ * 2/5/25
+ *
+ * Combined test case for VEB Tree and BST.
+ */
 
 #include <iostream>
 #include <set>
@@ -90,7 +95,6 @@ int main(int argc, char** argv) {
 	std::cout << "[BST] Time to successor query " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
 
 	#pragma endregion
-	*/
 
 	#pragma region VEBTree
 
@@ -138,52 +142,50 @@ int main(int argc, char** argv) {
 	#pragma region SUCESSSOR_TEST
 
 	std::cout << "\nBeginning VEB successor accuracy test... \n" << std::endl;
-	for (int i = 0; i < 100; i++) {
-		VEBTree veb(32);
+	VEBTree veb(32);
 
-		// Generate 2^14 random numbers [0 - 2^16).
-		const uint32_t num_elements = 1 << 14;
-		uint32_t* test_numbers = (uint32_t*)malloc(num_elements * sizeof(uint32_t));
-		if (!test_numbers) {
-			std::cerr << "Malloc test_numbers failed.\n";
-			exit(0);
-		}
-		safe_rand_bytes((unsigned char*)test_numbers, sizeof(*test_numbers) * num_elements);
-
-		// Insert them into veb and bst.
-		std::set<uint32_t> bst_reference;
-		for (uint32_t i = 0; i < num_elements; i++) {
-			veb.insert(test_numbers[i]);
-			bst_reference.insert(test_numbers[i]);
-		}
-
-		// Generate another 2^14 random numbers for successor queries.
-		uint32_t* successor_queries = (uint32_t*)malloc(num_elements * sizeof(uint32_t));
-		if (!successor_queries) {
-			std::cerr << "Malloc succ_queries failed.\n";
-			exit(0);
-		}
-		safe_rand_bytes((unsigned char*)successor_queries, sizeof(*successor_queries) * num_elements);
-
-		// Test successor, verifying the validity of the query results against known values in the tree.
-		for (uint32_t i = 0; i < num_elements; i++) {
-			uint32_t veb_successor = veb.successor(successor_queries[i]);
-			auto bst_successor = bst_reference.upper_bound(successor_queries[i]);
-
-			if ((bst_successor == bst_reference.end() && veb_successor != -1) ||
-				(bst_successor != bst_reference.end() && veb_successor != *bst_successor)) {
-				std::cerr << "[VEBTree] Successor mismatch for item " + std::to_string(i) + ": " << successor_queries[i]
-					<< ". Expected: " << (bst_successor != bst_reference.end() ? std::to_string(*bst_successor) : "empty")
-						<< ", Got: " << veb_successor << "\n";
-					exit(0);
-			}
-		}
-
-		std::cout << "[VEBTree] Successor accuracy test " + std::to_string(i + 1) + "/100 passed for " << num_elements << " queries.\n";
-
-		free(test_numbers);
-		free(successor_queries);
+	// Generate 2^14 random numbers [0 - 2^16).
+	const uint32_t num_elements = 1 << 14;
+	uint32_t* test_numbers = (uint32_t*)malloc(num_elements * sizeof(uint32_t));
+	if (!test_numbers) {
+		std::cerr << "Malloc test_numbers failed.\n";
+		exit(0);
 	}
+	safe_rand_bytes((unsigned char*)test_numbers, sizeof(*test_numbers) * num_elements);
+
+	// Insert them into veb and bst.
+	std::set<uint32_t> bst_reference;
+	for (uint32_t i = 0; i < num_elements; i++) {
+		veb.insert(test_numbers[i]);
+		bst_reference.insert(test_numbers[i]);
+	}
+
+	// Generate another 2^14 random numbers for successor queries.
+	uint32_t* successor_queries = (uint32_t*)malloc(num_elements * sizeof(uint32_t));
+	if (!successor_queries) {
+		std::cerr << "Malloc succ_queries failed.\n";
+		exit(0);
+	}
+	safe_rand_bytes((unsigned char*)successor_queries, sizeof(*successor_queries) * num_elements);
+
+	// Test successor, verifying the validity of the query results against known values in the tree.
+	for (uint32_t i = 0; i < num_elements; i++) {
+		uint32_t veb_successor = veb.successor(successor_queries[i]);
+		auto bst_successor = bst_reference.lower_bound(successor_queries[i]);
+
+		if ((bst_successor == bst_reference.end() && veb_successor != -1) ||
+			(bst_successor != bst_reference.end() && veb_successor != *bst_successor)) {
+			std::cerr << "[VEBTree] Successor mismatch for item " + std::to_string(i) + ": " << successor_queries[i]
+				<< ". Expected: " << (bst_successor != bst_reference.end() ? std::to_string(*bst_successor) : "empty")
+					<< ", Got: " << veb_successor << "\n";
+				exit(0);
+		}
+	}
+
+	std::cout << "[VEBTree] Successor accuracy test " + std::to_string(i + 1) + "/100 passed for " << num_elements << " queries.\n";
+
+	free(test_numbers);
+	free(successor_queries);
 
 	#pragma endregion
 
